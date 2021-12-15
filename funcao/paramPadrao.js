02d87505df5d24f3b2de19a7de0daa10191a0cee
braiancalot <calot665@hotmail.com>
2021-12-15 09:02:38 -0300

    More

--- funcao/paramPadrao.js
+++ funcao/paramPadrao.js
@@ -1,3 +1,4 @@
+// estrategia 1 para parametros padrao
 function soma1(a, b, c){
     a = a || 1
     b = b || 1
@@ -5,19 +6,21 @@ function soma1(a, b, c){
     return a + b + c
 }
 
-console.log(soma1())
-console.log(soma1(3))
-console.log(soma1(1, 2, 3))
-console.log(soma1(0, 0, 0))
+console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
 
+// estrategia 2, 3 e 4
 function soma2(a, b, c){
     a = a !== undefined ? a : 1
     b = 1 in arguments ? b : 1
-    c = isNaN(c) ? 1 : c
+    c = isNaN(c) ? 1 : c  // SEGUNDA MELHOR OPÇÂO!!!
     return a + b + c
 }
 
-console.log(soma2())
-console.log(soma2(3))
-console.log(soma2(1, 2, 3))
-console.log(soma2(0, 0, 0))
+console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
+
+// valor padrao do es2015  / MELHOR OPÇÂO!!!!
+function soma3(a = 1, b = 1, c = 1){
+    return a + b + c
+}
+
+console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(0, 0, 0))
