5444f756276804cddeb717dca32eb728fbdcf2cc
braiancalot <calot665@hotmail.com>
2021-12-30 22:59:07 -0300

    more

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Waleska', 'Siblesca']
+const filhos = ['Washiton', 'Wesclei']
+const todos = filhas.concat(filhos, 'Fulano')
+
+console.log(todos, filhas, filhos)
+
+console.log([].concat([2, 3], [4, 5], 6, [[7, 8]]))
