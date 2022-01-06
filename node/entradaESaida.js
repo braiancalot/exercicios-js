7a763bb777924affc2b4329f8da57a95a161ef50
braiancalot <calot665@hotmail.com>
2022-01-06 00:23:36 -0300

    more

---
+++ node/entradaESaida.js
@@ -0,0 +1,16 @@
+const os = require('os')
+const anonimo = process.argv.indexOf('-a') !== -1
+// console.log(anonimo)
+
+if(anonimo){
+    process.stdout.write(`Fala, Anônimo!${os.EOL}`)  // Pra funcionar: Substituir o "\n" por "${os.EOL}". Para isso é necessário: "require('os')"
+    process.exit()
+} else {
+    process.stdout.write('Informe o seu nome: ')
+    process.stdin.on('data', data => {
+        const nome = data.toString().replace(`${os.EOL}`, '')
+
+        process.stdout.write(`Fala, ${nome}!${os.EOL}`)
+        process.exit()
+    })
+}
