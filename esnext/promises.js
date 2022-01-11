e8cbc24aa57d22385d4d3ed6a6c16c922a55d7ff
braiancalot <calot665@hotmail.com>
2022-01-10 22:39:19 -0300

    more

--- esnext/promises.js
+++ esnext/promises.js
@@ -9,3 +9,4 @@ function falarDepoisDe(segundos, frase) {
 falarDepoisDe(3, 'Que Legal!')
     .then(frase => frase.concat('?!?'))
     .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e))
